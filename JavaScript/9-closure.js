559fe0b30e01a658548971de7743172ef1f62050
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-02-01 23:48:40 +0200

    Add most safe solution

---
+++ JavaScript/9-closure.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Rectangle = (() => {
+  const hidden = new Map();
+
+  return class {
+    constructor(height, width) {
+      hidden.set(this, { height, width });
+    }
+
+    get area() {
+      const { height, width } = hidden.get(this);
+      return height * width;
+    }
+  };
+})();
+
+// Usage
+
+const rect = new Rectangle(100, 200);
+console.dir({ rect });
+console.log('area:', rect.area);
