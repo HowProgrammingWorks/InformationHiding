1e2eaeb2079b7a079fa7929aa60f155b8e5aaa52
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-02-02 00:23:13 +0200

    Add real private members

---
+++ JavaScript/a-private.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Rectangle {
+  #height
+  #width
+
+  constructor(height, width) {
+    this.#height = height;
+    this.#width = width;
+  }
+
+  get area() {
+    return this.#height * this.#width;
+  }
+}
+
+// Usage
+
+const rect = new Rectangle(100, 200);
+console.dir({ rect });
+console.log('area:', rect.area);
